c119991479dae15d25f9d1417d18587aa4580435
FarrelSZ <farel.it12@gmail.com>
2026-05-03 14:26:47 +0700

    refactor JWT token handling to include refresh token generation and update expiration time for access tokens

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,15 +1,27 @@
 import jwt from "jsonwebtoken";
-import { SECRET } from "./env";
+import { REFRESH_SECRET, SECRET } from "./env";
 import { IUserToken } from "./interface";
 
 export const generateToken = (payload: IUserToken) => {
   const token = jwt.sign(payload, SECRET, {
-    expiresIn: "2d",
+    expiresIn: "1h",
   });
   return token;
 };
 
 export const verifyToken = (token: string) => {
   const user = jwt.verify(token, SECRET) as IUserToken;
+  return user;
+};
+
+export const generateRefreshToken = (payload: IUserToken) => {
+  const token = jwt.sign(payload, REFRESH_SECRET, {
+    expiresIn: "30d",
+  });
+  return token;
+};
+
+export const verifyRefreshToken = (token: string) => {
+  const user = jwt.verify(token, REFRESH_SECRET) as IUserToken;
   return user;
 };
