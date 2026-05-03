e3e9e42dc4650b46406437de79bb04e642e0a1fa
FarrelSZ <farel.it12@gmail.com>
2026-05-03 14:24:24 +0700

    initialize server with database and Redis connection, add middleware for CORS, JSON parsing, and cookie handling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import router from "./routes/api";
+import connect from "./utils/database";
+import { connectRedis } from "./utils/redis";
+import cors from "cors";
 
 async function init() {
   try {
     const app = express();
     const PORT = 3000;
+
+    await connect();
+    console.log("Database connected!");
+
+    await connectRedis();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use("/api", router);
 
     app.get("/", (req, res) => {
       res.status(200).json({ message: "Server Menyala" });
