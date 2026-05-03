06754ca8b5e547473e71da34771d4e03e0d80bd1
FarrelSZ <farel.it12@gmail.com>
2026-05-03 14:25:05 +0700

    add environment variables for Redis configuration and secrets

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,3 +4,7 @@ dotenv.config();
 
 export const DATABASE_URL = process.env.DATABASE_URL || "";
 export const SECRET = process.env.SECRET || "";
+export const REFRESH_SECRET = process.env.REFRESH_SECRET || "";
+export const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+export const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379");
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD || "";
